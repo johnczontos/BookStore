0247afbf62b6c2c770ef6a44c91024ab8bacbf57
richa <richa@RICKYS-MONEY-PI>
2022-05-09 00:41:13 -0700

    Add project files.

---
+++ BookStore/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+using BookStore.ViewModel;
+using BookStore.View;
+
+namespace BookStore
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            MainPage window = new MainPage();
+            UserViewModel VM = new UserViewModel();
+            window.DataContext = VM;
+            window.Show();
+        }
+    }
+
+   
+}
+
+
